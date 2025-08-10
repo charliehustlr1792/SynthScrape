e8c4df958d12046c823ef2f67a74c2da78ceb3d9
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2025-08-11 03:49:21 +0530

    fix:themeing issues

--- src/components/providers/AppProviders.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import {ThemeProvider} from "next-themes"
-export function AppProviders({children}:{children:React.ReactNode}){
-    return(
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {children}
-        </ThemeProvider>
-    )
-}
