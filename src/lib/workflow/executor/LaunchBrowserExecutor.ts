4d92e6e4d170a12c875c4752b7487833296df536
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2025-08-19 13:38:35 +0530

    feat:execution viewer

--- src/lib/workflow/executor/LaunchBrowserExecutor.ts
+++ src/lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -8,10 +8,12 @@ export async function LaunchBrowserExecutor(environment:ExecutionEnvironment<typ
         const browser=await puppeteer.launch({
             headless:true
         })
+        environment.log.info("Browser started successfully")
         environment.setBrowser(browser)
         const page=await browser.newPage()
         await page.goto(websiteUrl)
         environment.setPage(page)
+        environment.log.info(`Opened page at: ${websiteUrl}`)
         return true;
     }catch(error:any){
         environment.log.error(error.message)
