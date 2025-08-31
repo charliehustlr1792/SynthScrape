a63a8e3d1a4a45c854868ec48fff2f16e2a26e52
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2025-08-31 22:29:46 +0530

    fix:build errors

--- src/app/(dashboard)/(home)/page.tsx
+++ src/app/(dashboard)/(home)/page.tsx
@@ -11,33 +11,38 @@ import ExecutionStatusChart from './_components/ExecutionStatusChart';
 import { GetCreditsUsageInPeriod } from '../../../../actions/analytics/getCreditsUsageInPeriod';
 import CreditsUsageChart from '../billing/_components/CreditsUsageChart';
 
-const HomePage
- = ({searchParams}:{searchParams:{month?:string;
-  year?:string
- }}) => {
-  const currentDate=new Date()
-  const {month,year}=searchParams
-  const period:Period={
-    month:month?parseInt(month):currentDate.getMonth(),
-    year:year?parseInt(year):currentDate.getFullYear()
+async function HomePage({
+  searchParams
+}: {
+  searchParams: Promise<{
+    month?: string;
+    year?: string;
+  }>
+}) {
+  const currentDate = new Date()
+  const { month, year } = await searchParams
+  const period: Period = {
+    month: month ? parseInt(month) : currentDate.getMonth(),
+    year: year ? parseInt(year) : currentDate.getFullYear()
   }
+  
   return (
     <div className='flex flex-1 flex-col h-full'>
       <div className='flex justify-between'>
-        <h1 className='text-3xl font-bol'>Home</h1>
-          <Suspense fallback={<Skeleton className='w-[180px] h-[40px]'/>}>
-            <PeriodSelectorWrapper selectedPeriod={period}/>
-          </Suspense>
+        <h1 className='text-3xl font-bold'>Home</h1>
+        <Suspense fallback={<Skeleton className='w-[180px] h-[40px]'/>}>
+          <PeriodSelectorWrapper selectedPeriod={period}/>
+        </Suspense>
       </div>
       <div className="h-full py-6 flex flex-col gap-4">
         <Suspense fallback={<StatsCardSkeleton/>}>
-        <StatsCards selectedPeriod={period}/>
+          <StatsCards selectedPeriod={period}/>
         </Suspense>
         <Suspense fallback={<Skeleton className='w-full h-[300px]'/>}>
-        <StatsExecutionStatus selectedPeriod={period}/>
+          <StatsExecutionStatus selectedPeriod={period}/>
         </Suspense>
         <Suspense fallback={<Skeleton className='w-full h-[300px]'/>}>
-        <CreditsUsageInPeriod selectedPeriod={period}/>
+          <CreditsUsageInPeriod selectedPeriod={period}/>
         </Suspense>
       </div>
     </div>
@@ -46,29 +51,28 @@ const HomePage
 
 
 async function PeriodSelectorWrapper({selectedPeriod}:{selectedPeriod:Period}){
-  const periods=await GetPeriods()
-  return<PeriodSelector selectedPeriod={selectedPeriod} periods={periods} />
+  const periods = await GetPeriods()
+  return <PeriodSelector selectedPeriod={selectedPeriod} periods={periods} />
 }
 
-
 async function StatsCards({selectedPeriod}:{selectedPeriod:Period}){
-  const data=await GetStatsCardsValue(selectedPeriod)
+  const data = await GetStatsCardsValue(selectedPeriod)
   return (
     <div className='grid gap-3 lg:gap-8 lg:grid-cols-3 min-h-[120px]'>
       <StatsCard 
-      title="Workflow executions"
-      value={data.worklowExecutions}
-      icon={CirclePlayIcon}
+        title="Workflow executions"
+        value={data.worklowExecutions}
+        icon={CirclePlayIcon}
       />
       <StatsCard 
-      title="Phase executions"
-      value={data.phaseExecutions}
-      icon={WaypointsIcon}
+        title="Phase executions"
+        value={data.phaseExecutions}
+        icon={WaypointsIcon}
       />
       <StatsCard 
-      title="Credits consumed"
-      value={data.creditsConsumed}
-      icon={CoinsIcon}
+        title="Credits consumed"
+        value={data.creditsConsumed}
+        icon={CoinsIcon}
       />
     </div>
   )
@@ -84,14 +88,14 @@ function StatsCardSkeleton(){
   )
 }
 
-
 async function StatsExecutionStatus({selectedPeriod}:{selectedPeriod:Period}){
-  const data=await GetWorkflowExecutionStats(selectedPeriod)
+  const data = await GetWorkflowExecutionStats(selectedPeriod)
   return <ExecutionStatusChart data={data}/>
 }
 
 async function CreditsUsageInPeriod({selectedPeriod}:{selectedPeriod:Period}){
-  const data=await GetCreditsUsageInPeriod(selectedPeriod)
+  const data = await GetCreditsUsageInPeriod(selectedPeriod)
   return <CreditsUsageChart data={data} title="Daily credits spent" description="Daily credit consumed in selected period"/>
 }
+
 export default HomePage
