5bd577785e10ece28504881474e3f2abd49834b6
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2025-09-05 03:20:41 +0530

    fix:builderrors

--- actions/billing/getUserPurchaseHistory.ts
+++ actions/billing/getUserPurchaseHistory.ts
@@ -1,7 +1,5 @@
-import prisma from '@/lib/prisma';
-import { UserPurchase } from './../../src/generated/prisma/index.d';
 "use server"
-
+import prisma from '@/lib/prisma';
 import { auth } from "@clerk/nextjs/server"
 
 export async function GetUserPurchaseHistory(){
