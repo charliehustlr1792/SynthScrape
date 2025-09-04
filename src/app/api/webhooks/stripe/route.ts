163d6097cad743711bd9ff1e7c11d354ed09ad3f
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2025-09-04 22:33:32 +0530

    feat:setting up stripe

---
+++ src/app/api/webhooks/stripe/route.ts
@@ -0,0 +1,20 @@
+/* import { stripe } from "@/lib/stripe/stripe"
+import { headers } from "next/headers"
+import { NextResponse } from "next/server"
+
+export async function POST(request:Request){
+    const body=await request.text()
+    const signature=(await headers()).get("stripe-signature") as string
+
+    try{
+        const event=stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET
+        )
+
+        return new NextResponse(null,{status:200})
+    }catch(error){
+        console.error("stripe webhook error")
+    }
+} */
--- types/billing.ts
+++ types/billing.ts
@@ -9,7 +9,8 @@ export type CreditsPack={
     name:string;
     label:string;
     credits:number;
-    price:number
+    price:number;
+    priceId:string
 }
 
 export const CreditsPack:CreditsPack[]=[
@@ -18,21 +19,24 @@ export const CreditsPack:CreditsPack[]=[
         name:"Small Pack",
         label:"1,000 credits",
         credits:1000,
-        price:999//$9.99
+        price:999,//$9.99
+        priceId:process.env.STRIPE_SMALL_PACK_ID!
     },
     {
         id:PackId.MEDIUM,
         name:"Medium Pack",
         label:"5,000 credits",
         credits:5000,
-        price:3999//$39.99
+        price:3999,//$39.99
+        priceId:process.env.STRIPE_MEDIUM_PACK_ID!
     },
     {
         id:PackId.LARGE,
         name:"Large Pack",
         label:"10,000 credits",
         credits:10000,
-        price:6999//$69.99
+        price:6999,
+        priceId:process.env.STRIPE_LARGE_PACK_ID!//$69.99
     }
 ]
 
