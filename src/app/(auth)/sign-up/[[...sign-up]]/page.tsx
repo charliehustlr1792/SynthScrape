a0c98407fb0b3b289dc70c3fd9b58d3a9c56a6eb
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2025-11-29 18:18:20 +0530

    fix auth issues

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
 import { SignUp } from '@clerk/nextjs'
+import { useUser } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Page() {
+  const { isSignedIn } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push('/dashboard');
+    }
+  }, [isSignedIn, router]);
+
   return <SignUp />
 }
