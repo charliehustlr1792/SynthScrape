d2ae5ec22c51c8a6fbf040ac2c3b4aba436f5713
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2025-12-24 04:30:17 +0530

    fix :build issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,24 @@
-import { PrismaClient } from "@/generated/prisma";
-const prisma =new PrismaClient();
-export default prisma;
+// lib/prisma.ts
+import { PrismaClient } from '@/generated/prisma'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
